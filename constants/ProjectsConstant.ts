f9d5c9eb794a19ee39525d8954453c73c589d4c0
Sanjida Nasrin Tanju <86721558+sntanju@users.noreply.github.com>
2025-01-12 20:58:08 +0600

    updated

--- constants/ProjectsConstant.ts
+++ constants/ProjectsConstant.ts
@@ -2,21 +2,16 @@ export const PROJECT_HEADING = 'my Project';
 
 export const projectsData = [
   {
-    title: 'Aladin',
+    title: 'Blog Publishing Site',
     description:
-      'Aladin is a tool by Exabyting to help companies automate employee attendance, streamline leave management, check-in/check-out on each working day.',
-    tags: ['React', 'Context API', 'Chakra UI'],
+      'Blog publishing site is a project where users can sign up with email verification, post their own blogs, and vote for blogs published on the site. This project demonstrates my ability to implement user authentication, content creation, and interactive features.',
+    tags: ['Laravel', 'MYSQL', 'Blade'],
   },
   {
-    title: 'Identity Management Platform',
+    title: 'Typer',
     description:
-      'This project works for all sorts of identities of bKash Customers, PRA, Merchants and Youth by manually checking of all identities, calculating a score based on given info of, saving all info of users.',
-    tags: ['React', 'TypeScript', 'Context API', 'Chakra UI'],
+      'Typer is a project where users can log in and take a typing test using random quotes. This project demonstrates my ability to implement user authentication, dynamic content generation, and interactive user experiences.',
+    tags: ['React', 'NodeJS', 'HTML', 'Bootstrap'],
   },
-  {
-    title: 'Nominee',
-    description:
-      'This is the feature which is a tiny part of identity management platform. This is integrated with bKash App ',
-    tags: ['React', 'Chakra UI', 'Context API'],
-  },
+  
 ];
