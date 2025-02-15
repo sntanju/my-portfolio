cf8a4157c31aa64a4f563a2448afe26368558fbc
Sanjida Nasrin Tanju <86721558+sntanju@users.noreply.github.com>
2025-02-15 23:21:52 +0600

    added hackernews project data

--- constants/ProjectsConstant.ts
+++ constants/ProjectsConstant.ts
@@ -8,6 +8,12 @@ export const projectsData = [
     tags: ['Laravel', 'MYSQL', 'Blade'],
   },
   {
+    title: 'Hacker News',
+    description:
+      'Hacker News is a project that fetches and displays real-time top news using the Hacker News API. Users can view article details, including scores, authors, and comment counts. This project showcases my skills in API integration, dynamic data rendering, and building interactive, responsive UIs with Next JS.',
+    tags: ['NextJS', 'Firebase SDK', 'Tailwind'],
+  },
+  {
     title: 'Typer',
     description:
       'Typer is a project where users can log in and take a typing test using random quotes. This project demonstrates my ability to implement user authentication, dynamic content generation, and interactive user experiences.',
